a559dc58861780cbe95d8f8b1c11c5fa21985cda
James Heggs <james.heggs@closconsultancy.com>
2018-10-18 11:05:48 +0100

    Code in example solutions

--- app/alarm.js
+++ app/alarm.js
@@ -11,19 +11,28 @@ function wakeup(currentTime) {
   // https://www.w3schools.com/jsref/jsref_obj_date.asp
   const currentHour = currentTime.getHours();
 
-  if (currentHour > 12) {
-    return "Ugggggh its too early - I'm not waking up yet";
-  } else {
-    return "Get up!! You're wasting the day";
-  }
+    switch (currentHour) {
+        case 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11: {
+            return "Ugggggh its too early - I'm not waking up yet";
+        }
+        case 12, 13: {
+            return "Yessssss its lunch time!!!";
+        }
+        case 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24: {
+            return "Get up!! You're wasting the day";
+        }
+
+    }
 }
 
 function snooze() {
   let snoozeAttempts = 0;
 
-  for (let i = 0; i < 10; i++) {
-    snoozeAttempts++;
-  }
+    let snoozeAttempts = 0;
+
+    while(snoozeAttempts < 20 ) {
+        snoozeAttempts++;
+    }
 
   return snoozeAttempts;
 }
--- test/alarm.test.js
+++ test/alarm.test.js
@@ -6,6 +6,12 @@ test('Moans if too early', () => {
   expect(alarm.wakeup(timeNow)).toBe("Ugggggh its too early - I'm not waking up yet");
 });
 
+test('Its lunchtime', () => {
+  let timeNow = new Date();
+  timeNow.setHours(13);
+  expect(alarm.wakeup(timeNow)).toBe("Yessssss its lunch time!!!");
+});
+
 test('Snoozing for a set amount', () => {
-  expect(alarm.snooze()).toBe(10);
+  expect(alarm.snooze()).toBe(20);
 });
