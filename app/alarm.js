f5d09a1b0e46ffa099a355dbe62520cf136ec504
Harriet Ryder <harriethryder@gmail.com>
2019-03-19 13:16:28 +0000

    Add Linting

--- app/alarm.js
+++ app/alarm.js
@@ -1,33 +1,34 @@
-const peopleThatHaveUsedAlarm = [ { name: "James", wakeUpTime: 7 },
-                                  { name: "Susan", wakeUpTime: 13 },
-                                  { name: "Geoff", wakeUpTime: 14 },
-                                  { name: "Lisa", wakeUpTime: 6 }];
+// eslint-disable-next-line no-unused-vars
+const peopleThatHaveUsedAlarm = [
+  { name: "James", wakeUpTime: 7 },
+  { name: "Susan", wakeUpTime: 13 },
+  { name: "Geoff", wakeUpTime: 14 },
+  { name: "Lisa", wakeUpTime: 6 }
+];
 
 function wakeup(currentTime) {
-    // currentTime will be a Javascript Date object
-    // https://www.w3schools.com/jsref/jsref_obj_date.asp
-    const currentHour = currentTime.getHours();
+  // currentTime will be a Javascript Date object
+  // https://www.w3schools.com/jsref/jsref_obj_date.asp
+  const currentHour = currentTime.getHours();
 
-    if(currentHour > 12 ) {
-        return "Ugggggh its too early - I'm not waking up yet";
-    }
-    else {
-        return "Get up!! You're wasting the day";
-    }
+  if (currentHour > 12) {
+    return "Ugggggh its too early - I'm not waking up yet";
+  } else {
+    return "Get up!! You're wasting the day";
+  }
 }
 
 function snooze() {
+  let snoozeAttempts = 0;
 
-    let snoozeAttempts = 0;
+  for (let i = 0; i < 10; i++) {
+    snoozeAttempts++;
+  }
 
-    for(let i = 0; i < 10; i++ ) {
-        snoozeAttempts++
-    }
-
-    return snoozeAttempts;
+  return snoozeAttempts;
 }
 
 module.exports = {
-    wakeup,
-    snooze
+  wakeup,
+  snooze
 };
